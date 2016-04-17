e9f0180ad0028d958f79d9eba0101c570ad11962
Brendan Carr <Brendan Carr>
2016-04-16 22:05:21 -0400

    fixing mongoose

--- app.js
+++ app.js
@@ -30,63 +30,63 @@ app.listen(app.get('port'), function () {
     console.log('\n');
 });
 
-mongoose.connect('mongodb://localhost/test');
-
-
-var db = mongoose.connection;
-
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-
-db.once('open', function () {
-    console.log("connect has been made")
-        // we're connected!
-});
-
-
-var mongoConnection = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/foobar');
-
-
-
+//mongoose.connect('mongodb://localhost/test');
 //
-
 //
-
-
-const http = require('http');
-
-
-const port = process.env.PORT || 1337;
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fooobar');
-
-let Post = mongoose.model('Post', {
-    title: String
-});
-
-let post = new Post({
-    title: 'My Awesome Goals for 2016!'
-});
-
-post.save(function (err) {
-    if (err) {
-        console.log(err);
-    }
-    console.log('Blog post saved!');
-});
-
-http.createServer((req, res) => {
-    Post.find((err, posts) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(posts);
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.end(JSON.stringify(posts));
-    })
-}).listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+//var db = mongoose.connection;
+//
+//
+//db.on('error', console.error.bind(console, 'connection error:'));
+//
+//
+//db.once('open', function () {
+//    console.log("connect has been made")
+//        // we're connected!
+//});
+//
+//
+//var mongoConnection = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/foobar');
+//
+//
+//
+////
+//
+////
+//
+//
+//const http = require('http');
+//
+//
+//const port = process.env.PORT || 1337;
+//
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fooobar');
+//
+//let Post = mongoose.model('Post', {
+//    title: String
+//});
+//
+//let post = new Post({
+//    title: 'My Awesome Goals for 2016!'
+//});
+//
+//post.save(function (err) {
+//    if (err) {
+//        console.log(err);
+//    }
+//    console.log('Blog post saved!');
+//});
+//
+//http.createServer((req, res) => {
+//    Post.find((err, posts) => {
+//        if (err) {
+//            return console.log(err);
+//        }
+//        console.log(posts);
+//        res.writeHead(200, {
+//            'Content-Type': 'application/json'
+//        });
+//        res.end(JSON.stringify(posts));
+//    })
+//}).listen(port, () => {
+//    console.log(`Server running at http://localhost:${port}`);
+//});
